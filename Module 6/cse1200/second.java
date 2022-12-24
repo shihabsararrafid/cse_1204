29102ac8586e21f6fbdec930c5d384fcc19ec6b8
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-12-24 08:23:43 +0600

    hello bd

--- Module 6/cse1200/second.java
+++
@@ -1,7 +0,0 @@
-package cse1200;
-
-public class second {
-    public static void main(String[] args) {
-        myPack.main(null);
-    }
-}
--- Module 6/first.java
+++
@@ -1,9 +0,0 @@
-package sa;
-
-class Simple {
-
-    public static void main(String[] args) {
-
-        System.out.println("My name is shihab sarar islam rafid \n");
-    }
-}
